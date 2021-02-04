456ba69322c8b1cf2f205b8ff7d93f683155d085
icephat <phattaraporn.sa@ku.th>
2021-02-04 16:41:07 +0700

    แก้ไข
    แก้ไขโดยให้มีตัวรับการ*100

--- 6320500603_7.c
+++ 6320500603_7.c
@@ -2,18 +2,19 @@
 int main()
 {
     float time;
-    int tmp,m;
+    int a,tmp,m;
     scanf("%f",&time);
-    tmp=time*100;
-    m=tmp%100;
-    tmp/=100;
+    tmp=time*100/100;
+    a=time*100;
+    m=a%100;
     if(tmp<=11)
     {
-        printf("%.2f a.m.",time);
+        printf("%d:%d a.m.",tmp,m);
     }
     else
     {
-        printf("%.2f p.m.",time);
+        printf("%d:%d p.m.",tmp,m);
     }
+
     return 0;
 }
